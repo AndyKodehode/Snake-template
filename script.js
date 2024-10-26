2eb357241bb325d531e67fb177b040ad6ae3c98b
AndyKodehode <andykodehode@gmail.com>
2024-10-26 22:08:46 +0200

    instrukser for snake

--- script.js
+++ script.js
@@ -1,1 +1,85 @@
-console.log('Hello!');
+
+// HTML 
+
+/*
+div for grid
+noe til å lagre score i
+start/stop knapp
+
+*/
+
+//CSS
+/*
+
+grid
+div
+snake
+eple
+
+*/
+
+
+//JS
+
+/*
+
+Utgangsverdier:
+
+  const width = 10 //Hvor mange divs som går i bredden
+  let currentIndex = 0 //so first div in our grid
+  let appleIndex = 0 //so first div in our grid
+  let currentSnake = [2,1,0] 
+  let direction = 1
+  let score = 0
+  let speed = 0.9
+  let intervalTime = 0
+  let interval = 0
+
+//Grid//
+
+Loop for å lage divs
+
+
+//Start, stop//
+
+
+for at slangen skal bevege seg automatisk må man 
+fjerne snake class og legge til en snake class med en intervall
+
+setInterval kan kalle på funksjonen hvor du har satt opp resten:
+- sjekk av krasj
+- endring av retning
+- sjekk om man treffer eple
+
+
+//Snake//
+
+Krasj
+
+Sett opp sjekk for om slangen treffer de forskjellige veggene
+Sett opp sjekk for om slangen treffer seg selv
+
+Beveglse
+
+Finn ut hvilken key du skal bruke til å gå høyre, venstre, opp og ned,
+sett keys til retning
+divs må fjernes og legges til for hver gang man flytter slangen
+
+
+Eple
+
+En funksjon som setter eple i random posisjoner på brettet
+
+Når slangen treffer eple så må eple fjernes fra posisjonen og legges til en ny posisjon
+Når slangen treffer eple må det legges til en div på halen 
+
+En funksjon som øker farten til slangen hver gang den treffer et eple
+
+//Regler og oppdateringer//
+
+Hva skjer når man treffer veggen? 
+
+Har man flere liv eller er man ute med en gang?
+
+*/
+
